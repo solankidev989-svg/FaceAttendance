e55729f0fbbaafece2b721e955d79cf7fa8d4cae
solankidev989-svg <solankidev989@gmail.com>
2026-05-23 11:03:57 +0530

    Added Face Attendance project files
    Added Face Attendance project files

---
+++ main.py
@@ -0,0 +1,72 @@
+import cv2
+import face_recognition
+import os
+import numpy as np
+from datetime import datetime
+
+path = 'images'
+images = []
+classNames = []
+
+myList = os.listdir(path)
+
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+def findEncodings(images):
+    encodeList = []
+
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+
+def markAttendance(name):
+    with open('attendance.csv', 'a+') as f:
+        now = datetime.now()
+        dtString = now.strftime('%H:%M:%S')
+        f.writelines(f'\n{name},{dtString}')
+
+encodeListKnown = findEncodings(images)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+
+    imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+
+            y1,x2,y2,x1 = faceLoc
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.putText(img,name,(x1,y1-10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,(255,255,255),2)
+
+            markAttendance(name)
+
+    cv2.imshow('Webcam', img)
+
+    if cv2.waitKey(1) == 13:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
